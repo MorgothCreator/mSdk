b0bfb2803609a6ad9e12bf16e684c8e2b595c07e
Iulian Gheorghiu <morgoth.creator@gmail.com>
2013-05-06 13:59:48 +0300

    Modified for compatibility

--- SDK/platform/stm32f4xx/sys/system_stm32f4xx.c
+++ SDK/platform/stm32f4xx/sys/system_stm32f4xx.c
@@ -217,7 +217,7 @@ static void SetSysClock(void);
 void SystemInit(void)
 {
   /* FPU settings ------------------------------------------------------------*/
-  #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
+  #if (__FPU_PRESENT == 1) && defined(__FPU_USED)
     SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
   #endif
   /* Reset the RCC clock configuration to the default reset state ------------*/
