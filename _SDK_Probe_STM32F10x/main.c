b26f959906e8a2dbce98de42dd18dee532a52fce
Iulian Gheorghiu <morgoth.creator@gmail.com>
2013-05-10 20:46:16 +0300

    STM32F10x project update

---
+++ _SDK_Probe_STM32F10x/main.c
@@ -0,0 +1,38 @@
+/*
+ * main.c
+ *
+ *  Created on: May 9, 2013
+ *      Author: Iulian Gheorghiu (morgoth.creator@gmail.com)
+ */
+
+#include "board_init.h"
+
+int main(void)
+{
+	board_init();
+	register int CntDelay;
+	while(1)
+	{
+		CntDelay = 8000000;
+		do {
+			if(!CntDelay) break;
+			CntDelay--;
+		} while(1);
+		gpio_out(LED1, 1);
+		gpio_out(LED2, 1);
+		gpio_out(LED3, 1);
+		gpio_out(LED4, 1);
+		UARTprintf(DebugCom, "Str\n");
+	    CntDelay = 8000000;
+	    do {
+			if(!CntDelay) break;
+			CntDelay--;
+	    } while(1);
+		gpio_out(LED1, 0);
+		gpio_out(LED2, 0);
+		gpio_out(LED3, 0);
+		gpio_out(LED4, 0);
+		UARTprintf(DebugCom, "Rts\n");
+	}
+	return 0;
+}
