c9447deeb98d9a420175cd3e5e6eb38be2e5e722
Iulian Gheorghiu <morgoth.creator@gmail.com>
2013-05-25 22:42:44 +0300

    License update

--- SDK_Probe_AM335x/Beaglebone/main.c
+++ SDK_Probe_AM335x/Beaglebone/main.c
@@ -2,10 +2,10 @@
 /*    AM335x Multiplatform SDK test application.                                       */
 /*    Copyright (C) 2013  Iulian Gheorghiu.                                            */
 /*                                                                                     */
-/*    This program is free software: you can redistribute it and/or modify             */
-/*    it under the terms of the GNU General Public License as published by             */
-/*    the Free Software Foundation, either version 3 of the License, or                */
-/*    (at your option) any later version.                                              */
+/*    This program is free software; you can redistribute it and/or                    */
+/*    modify it under the terms of the GNU General Public License                      */
+/*    as published by the Free Software Foundation; either version 2                   */
+/*    of the License, or (at your option) any later version.                           */
 /*                                                                                     */
 /*    This program is distributed in the hope that it will be useful,                  */
 /*    but WITHOUT ANY WARRANTY; without even the implied warranty of                   */
