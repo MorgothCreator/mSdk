e64823fdbcba485a822390685e08e626be0d66c8
Iulian Gheorghiu <morgoth.creator@gmail.com>
2014-11-30 10:27:38 +0200

    Fix error when structure address is 0 or -1.

--- SDK/platform/am33/interface/gpio_interface.c
+++ SDK/platform/am33/interface/gpio_interface.c
@@ -92,7 +92,7 @@ new_gpio *_gpio_assign(unsigned int PortNr, unsigned int Pin, unsigned int Direc
 /*#####################################################*/
 void _gpio_free(new_gpio *gpio_struct)
 {
-	if(!gpio_struct || gpio_struct == -1) return;
+	if(!gpio_struct || gpio_struct == (new_gpio *)-1) return;
 	switch (gpio_struct->BaseAddr)
 	{
 	case GPIO0_addr:
@@ -113,7 +113,7 @@ void _gpio_free(new_gpio *gpio_struct)
 /*#####################################################*/
 bool _gpio_out(new_gpio *gpio_struct, unsigned int State)
 {
-	if(!gpio_struct || gpio_struct == -1) return false;
+	if(!gpio_struct || gpio_struct == (new_gpio *)-1) return false;
 	if(gpio_struct->Multipin) GPIOMultiplePinsWrite(gpio_struct->BaseAddr, State & gpio_struct->Pin, (~State) & gpio_struct->Pin);
 	else GPIOPinWrite(gpio_struct->BaseAddr, gpio_struct->Pin, State);
 	return true;
@@ -121,7 +121,7 @@ bool _gpio_out(new_gpio *gpio_struct, unsigned int State)
 /*#####################################################*/
 bool _gpio_direction(new_gpio *gpio_struct, unsigned int Direction)
 {
-	if(!gpio_struct || gpio_struct == -1) return false;
+	if(!gpio_struct || gpio_struct == (new_gpio *)-1) return false;
 	unsigned char cnt = 0;
 	unsigned int tmp = gpio_struct->Pin;
 	if(gpio_struct->Multipin)
@@ -144,20 +144,20 @@ bool _gpio_direction(new_gpio *gpio_struct, unsigned int Direction)
 /*#####################################################*/
 signed int _gpio_in(new_gpio *gpio_struct)
 {
-	if(!gpio_struct || gpio_struct == -1) return -1;
+	if(!gpio_struct || gpio_struct == (new_gpio *)-1) return -1;
 	if(gpio_struct->Multipin) return GPIOMultiplePinsRead(gpio_struct->BaseAddr, gpio_struct->Pin);
 	else return GPIOPinRead(gpio_struct->BaseAddr, gpio_struct->Pin)>>gpio_struct->Pin;
 }
 /*#####################################################*/
 bool _gpio_up_dn_enable(new_gpio *gpio_struct, bool enable)
 {
-	if(!gpio_struct || gpio_struct == -1) return false;
+	if(!gpio_struct || gpio_struct == (new_gpio *)-1) return false;
 	return true;//GPIOUpDnEnable(gpio_struct->BaseAddr, gpio_struct->PinNr, enable);
 }
 /*#####################################################*/
 bool _gpio_up_dn(new_gpio *gpio_struct, unsigned char value)
 {
-	if(!gpio_struct || gpio_struct == -1) return false;
+	if(!gpio_struct || gpio_struct == (new_gpio *)-1) return false;
 	return true;//GPIOUpDn(gpio_struct->BaseAddr, gpio_struct->PinNr, value);
 }
 /*#####################################################*/
