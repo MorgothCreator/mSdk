ab55d508ce186e8dbca3916bbba878271186a618
Morgoth <morgoth.creator@gmail.com>
2014-12-26 11:23:35 +0200

    Begin to add support for TDA19988 HDMI interface device.

---
+++ SDK/lib/device/tda19988.c
@@ -0,0 +1,26 @@
+/*
+ * tda19988.c
+ *
+ *  Created on: Dec 24, 2014
+ *      Author: Morgoth
+ */
+
+#include "stdbool.h"
+#include "api/gpio_def.h"
+#include "api/gpio_api.h"
+#include "include/hw/soc_AM335x.h"
+#include "include/hw/hw_control_AM335x.h"
+#include "include/hw/hw_types.h"
+
+bool tda19988_init()
+{
+    HWREG(SOC_CONTROL_REGS + CONTROL_CONF_XDMA_EVENT_INTR(0)) =
+              (2 << CONTROL_CONF_XDMA_EVENT_INTR0_CONF_XDMA_EVENT_INTR0_MMODE_SHIFT)    |
+              (1 << CONTROL_CONF_XDMA_EVENT_INTR0_CONF_XDMA_EVENT_INTR0_PUDEN_SHIFT)    |
+              (0 << CONTROL_CONF_XDMA_EVENT_INTR0_CONF_XDMA_EVENT_INTR0_PUTYPESEL_SHIFT)|
+              (1 << CONTROL_CONF_XDMA_EVENT_INTR0_CONF_XDMA_EVENT_INTR0_RXACTIVE_SHIFT) |
+              (0 << CONTROL_CONF_XDMA_EVENT_INTR0_CONF_XDMA_EVENT_INTR0_SLEWCTRL_SHIFT);
+
+}
+
+
---
+++ SDK/lib/device/tda19988.h
@@ -0,0 +1,15 @@
+/*
+ * tda19988.h
+ *
+ *  Created on: Dec 24, 2014
+ *      Author: Morgoth
+ */
+
+#ifndef TDA19988_H_
+#define TDA19988_H_
+
+
+
+
+
+#endif /* TDA19988_H_ */
