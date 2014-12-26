fe657beec7b5db983d57a4ae7ad9239bc432ccc4
Morgoth <morgoth.creator@gmail.com>
2014-12-26 11:16:43 +0200

    Add sysdelay support.

--- SDK/platform/stm32f10x/sys/sysdelay.c
+++ SDK/platform/stm32f10x/sys/sysdelay.c
@@ -6,6 +6,8 @@
  */
 
 #include "sysdelay.h"
+#include "board_properties.h"
+#include "sys/core_cm3.h"
 
 /******************************************************************************
 **                      INTERNAL VARIABLE DEFINITIONS
@@ -15,7 +17,7 @@ static volatile unsigned long long STimerCnt;
 //#####################################################
 void SysDelayTimerSetup(void)
 {
-
+	SysTick_Config((CoreFreq_Mhz * 1000000) / 1000);
 }
 //#####################################################
 void Sysdelay(unsigned int milliSec)
@@ -24,6 +26,15 @@ void Sysdelay(unsigned int milliSec)
 	while(flagIsr < STimerCnt);
 }
 //#####################################################
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+	STimerCnt++;
+}
 //ISR(RTC_OVF_vect)
 //{
 //	STimerCnt++;
