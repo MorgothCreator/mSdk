2e3ec04c4f17ca0f5466f771626fa00ce5ae4175
Morgoth <morgoth.creator@gmail.com>
2014-12-27 20:43:36 +0200

    Adapt the main.c to new SDK changes.

--- SDK_Probe_STM32F10x/main.c
+++ SDK_Probe_STM32F10x/main.c
@@ -25,24 +25,24 @@ int main(void)
 			ms5611_prom_data.C7);
 	while(1)
 	{
-		CntDelay = 5700000;
+		CntDelay = 2900000;
 		do {
 			if(!CntDelay) break;
 			CntDelay--;
 		} while(1);
 		gpio_out(LED1, 1);
-		UARTprintf(DebugCom, "Str\n");
-	    CntDelay = 5700000;
+		//UARTprintf(DebugCom, "Str\n");
+	    CntDelay = 2900000;
 	    do {
 			if(!CntDelay) break;
 			CntDelay--;
 	    } while(1);
 		gpio_out(LED1, 0);
-		UARTprintf(DebugCom, "Rts\n");
+		//UARTprintf(DebugCom, "Rts\n");
 		signed int Preasure = 0, Temperature = 0;
 		memset(&ms5611_prom_data, 0, sizeof(ms5611_prom_data));
 		ms5611_init(&ms5611_prom_data, TWI[0]);
-		UARTprintf(DebugCom, "Prom data: W0=%d, W1=%d, W2=%d, W3=%d, W4=%d, W5=%d, W6=%d, W7=%d\n\r",
+		UARTprintf(DebugCom, "Prom data:W0=%d,W1=%d,W2=%d,W3=%d,W4=%d,W5=%d,W6=%d,W7=%d\n\r",
 				ms5611_prom_data.C0,
 				ms5611_prom_data.C1,
 				ms5611_prom_data.C2,
@@ -55,7 +55,7 @@ int main(void)
 			UARTprintf(DebugCom, "Preasure = %d, Temperature = %d\n", Preasure, Temperature);
 		}
 		ms5611_display_preasure_result(&ms5611_prom_data, TWI[0], MS5611_CONVERT_OSR_1024);
-		UARTprintf(DebugCom, "ADC1 CH0 = %d, ADC1 CH1 = %d, ADC1 TempSensor = %d\n", ADC[0]->ConvResult[0], ADC[0]->ConvResult[1], ADC[0]->ConvResult[2]);
+		UARTprintf(DebugCom, "ADC1 CH0 = %d, ADC1 CH1 = %d, ADC1 TempSensor = %d\n\n", ADC[0]->ConvResult[0], ADC[0]->ConvResult[1], ADC[0]->ConvResult[2]);
 	}
 	return 0;
 }
