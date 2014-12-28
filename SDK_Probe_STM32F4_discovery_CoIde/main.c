01cdf11aef0a4a0cbd09e75fa8586bca7bbd8d70
Iulian Gheorghiu <morgoth.creator@gmail.com>
2014-12-28 21:01:23 +0200

    Remove inutile functions from main.c

--- SDK_Probe_STM32F4_discovery_CoIde/main.c
+++ SDK_Probe_STM32F4_discovery_CoIde/main.c
@@ -13,15 +13,6 @@ int main(void)
 	volatile int CntDelay;
 	ms5611_prom_data ms5611_prom_data;
 	ms5611_init(&ms5611_prom_data, TWI[0]);
-	UARTprintf(DebugCom, "Prom data: W0=%d, W1=%d, W2=%d, W3=%d, W4=%d, W5=%d, W6=%d, W7=%d\n\r",
-			ms5611_prom_data.C0,
-			ms5611_prom_data.C1,
-			ms5611_prom_data.C2,
-			ms5611_prom_data.C3,
-			ms5611_prom_data.C4,
-			ms5611_prom_data.C5,
-			ms5611_prom_data.C6,
-			ms5611_prom_data.C7);
 	while(1)
 	{
 		CntDelay = 6200000;
@@ -44,21 +35,6 @@ int main(void)
 		gpio_out(LED[2], 0);
 		gpio_out(LED[3], 0);
 		//UARTprintf(DebugCom, "Rts\n");
-		signed int Preasure = 0, Temperature = 0;
-		memset(&ms5611_prom_data, 0, sizeof(ms5611_prom_data));
-		ms5611_init(&ms5611_prom_data, TWI[0]);
-		UARTprintf(DebugCom, "Prom data:W0=%d,W1=%d,W2=%d,W3=%d,W4=%d,W5=%d,W6=%d,W7=%d\n\r",
-				ms5611_prom_data.C0,
-				ms5611_prom_data.C1,
-				ms5611_prom_data.C2,
-				ms5611_prom_data.C3,
-				ms5611_prom_data.C4,
-				ms5611_prom_data.C5,
-				ms5611_prom_data.C6,
-				ms5611_prom_data.C7);
-		if(ms5611_read(&ms5611_prom_data, TWI[0], MS5611_CONVERT_OSR_1024, &Preasure, &Temperature)) {
-			UARTprintf(DebugCom, "Preasure = %d, Temperature = %d\n", Preasure, Temperature);
-		}
 		ms5611_display_preasure_result(&ms5611_prom_data, TWI[0], MS5611_CONVERT_OSR_1024);
 		UARTprintf(DebugCom, "ADC1 CH0 = %d, ADC1 CH1 = %d, ADC1 TempSensor = %d\n\n", ADC[0]->ConvResult[0], ADC[0]->ConvResult[1], ADC[0]->ConvResult[2]);
 	}
