1541dd5a69ee68305fdb71de8d9feef11917b113
Iulian Gheorghiu <morgoth.creator@gmail.com>
2014-12-28 20:42:01 +0200

    Begin to add ADC support to STM32F4, this is a beginning, is not working due a ADC or DMA result transfer issue.

--- SDK/platform/stm32f4xx/interface/adc_interface.h
+++ SDK/platform/stm32f4xx/interface/adc_interface.h
@@ -1,14 +1,22 @@
 /*
  * adc_interface.h
  *
- *  Created on: Dec 28, 2014
+ *  Created on: Dec 26, 2014
  *      Author: Iulian Gheorghiu
  */
 /*#####################################################*/
-#ifndef ADC_INTERFACE_DEF_H_
-#define ADC_INTERFACE_DEF_H_
+#ifndef ADC_INTERFACE_H_
+#define ADC_INTERFACE_H_
+#include "stdbool.h"
+#include "api/adc_def.h"
 /*#####################################################*/
-#define ADC_CHANNELS_NR_PER_UNIT	(19)
+bool _adc_init(Adc_t *Adc_s);
+
+
+/*#####################################################*/
+#ifdef HEADER_INCLUDE_C_FILES
+#include "adc_interface.c"
+#endif
 /*#####################################################*/
 #endif /* ADC_INTERFACE_H_ */
 /*#####################################################*/
