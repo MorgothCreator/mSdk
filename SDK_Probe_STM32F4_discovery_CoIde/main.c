8346e8e324b3cc0a6a774b93955344dd3ece64ea
Iulian Gheorghiu <morgoth.creator@gmail.com>
2014-12-31 16:12:21 +0200

    Change delays from loops to software timers.

--- SDK_Probe_STM32F4_discovery_CoIde/main.c
+++ SDK_Probe_STM32F4_discovery_CoIde/main.c
@@ -5,38 +5,53 @@
  *      Iulian Gheorghiu <morgoth.creator@gmail.com>
  */
 #include "board_init.h"
+#include "api/timer_api.h"
 #include "device/ms5611.h"
+#include "device/mpu60x0.h"
+#include "device/mhc5883.h"
 
 int main(void)
 {
 	board_init();
-	volatile int CntDelay;
+	timer(TimerReadSensors);
+    timer_interval(&TimerReadSensors, 1000);
+	timer(TimerBlinkLed);
+    timer_interval(&TimerBlinkLed, 1000);
 	ms5611_prom_data ms5611_prom_data;
 	ms5611_init(&ms5611_prom_data, TWI[0]);
+	mpu60x0_init(TWI[0], 0);
+	mhc5883_init(TWI[0]);
+	bool led_status = false;
 	while(1)
 	{
-		CntDelay = 6200000;
-		do {
-			if(!CntDelay) break;
-			CntDelay--;
-		} while(1);
-		gpio_out(LED[0], 1);
-		gpio_out(LED[1], 1);
-		gpio_out(LED[2], 1);
-		gpio_out(LED[3], 1);
-		//UARTprintf(DebugCom, "Str\n");
-	    CntDelay = 6200000;
-	    do {
-			if(!CntDelay) break;
-			CntDelay--;
-	    } while(1);
-		gpio_out(LED[0], 0);
-		gpio_out(LED[1], 0);
-		gpio_out(LED[2], 0);
-		gpio_out(LED[3], 0);
-		//UARTprintf(DebugCom, "Rts\n");
-		ms5611_display_preasure_result(&ms5611_prom_data, TWI[0], MS5611_CONVERT_OSR_1024);
-		UARTprintf(DebugCom, "ADC1 CH0 = %d, ADC1 CH1 = %d, ADC1 TempSensor = %d\n\n", ADC[0]->ConvResult[0], ADC[0]->ConvResult[1], ADC[0]->ConvResult[2]);
+		if(timer_tick(&TimerBlinkLed)) {
+			if(led_status) {
+				gpio_out(LED[0], 0);
+				gpio_out(LED[1], 0);
+				gpio_out(LED[2], 0);
+				gpio_out(LED[3], 0);
+				timer_interval(&TimerBlinkLed, 100);
+				timer_enable(&TimerBlinkLed);
+				led_status = false;
+			} else {
+				gpio_out(LED[0], 1);
+				gpio_out(LED[1], 1);
+				gpio_out(LED[2], 1);
+				gpio_out(LED[3], 1);
+				timer_interval(&TimerBlinkLed, 900);
+				timer_enable(&TimerBlinkLed);
+				led_status = true;
+			}
+		}
+
+		if(timer_tick(&TimerReadSensors)) {
+			ms5611_display_preasure_result(&ms5611_prom_data, TWI[0], MS5611_CONVERT_OSR_1024);
+			mpu60x0_temperature_display_result(TWI[0], 0);
+			mpu60x0_giroscope_display_result(TWI[0], 0);
+			mpu60x0_accelerometer_display_result(TWI[0], 0);
+			mhc5883_display_result(TWI[0]);
+			UARTprintf(DebugCom, "ADC1 CH0 = %d, ADC1 CH1 = %d, ADC1 TempSensor = %d\n\n", ADC[0]->ConvResult[0], ADC[0]->ConvResult[1], ADC[0]->ConvResult[2]);
+		}
 	}
 	return 0;
 }
