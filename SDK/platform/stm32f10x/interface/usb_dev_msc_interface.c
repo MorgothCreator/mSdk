97dcee5a84182031a1137aee020bdc399f16e764
Iulian Gheorghiu <morgoth.creator@gmail.com>
2014-12-31 16:11:31 +0200

    Add "_usb_msc_dev_media_change_state" to "usb_dev_msc_interface.c" to avoid compiler warning for undeclared function.

--- SDK/platform/stm32f10x/interface/usb_dev_msc_interface.c
+++ SDK/platform/stm32f10x/interface/usb_dev_msc_interface.c
@@ -1,4 +1,10 @@
+#include "stdbool.h"
 #include "usb_dev_msc_interface.h"
+
+void _usb_msc_dev_media_change_state(unsigned int instance, bool media_is_present)
+{
+
+}
 
 void _usb_msc_dev_init(unsigned int instance)
 {
--- SDK/platform/stm32f10x/interface/usb_dev_msc_interface.h
+++ SDK/platform/stm32f10x/interface/usb_dev_msc_interface.h
@@ -1,6 +1,8 @@
 #ifndef __usb_dev_msc_interface__
 #define __usb_dev_msc_interface__
 
+#include "stdbool.h"
+
 #define GET_SECTOR_COUNT 1
 #define GET_SECTOR_SIZE 2
 
@@ -13,6 +15,7 @@ typedef struct {
 
 extern USBD_DRV_RW_FUNC drv_rw_func;
 
+void _usb_msc_dev_media_change_state(unsigned int instance, bool media_is_present);
 void _usb_msc_dev_init(unsigned int instance);
 
 #endif
