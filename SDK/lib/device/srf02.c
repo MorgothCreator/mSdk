0eb6901fc05a5308be0ebe5220184b69d2c047f2
Iulian Gheorghiu <morgoth.creator@gmail.com>
2015-01-06 15:36:39 +0200

    Add "SRF02" ultrasonic range finder device driver in I2C bus mode.

---
+++ SDK/lib/device/srf02.c
@@ -0,0 +1,84 @@
+/*
+ * srf02.c
+ *
+ *  Created on: Jan 6, 2015
+ *      Author: Morgoth
+ */
+
+#include "stdbool.h"
+#include "srf02.h"
+#include "board_init.h"
+#include "api/twi_api.h"
+#include "api/timer_api.h"
+
+bool srf02_start(SRF02_t *structure, SRF02_COMMANDS command) {
+	if(structure->busy) return true;
+	timer_interval(&structure->Timeout_Timer, 80);
+	structure->busy = true;
+	Twi_t* TwiStruct = structure->TWI;
+	if(structure->addr)
+		TwiStruct->MasterSlaveAddr = structure->addr;
+	else
+		TwiStruct->MasterSlaveAddr = 0x70;
+
+    switch(command)
+    {
+    case SRF02_START_RANGING:
+    case SRF02_FAKE_RANGING:
+        TwiStruct->TxBuff[0] = 0;
+        TwiStruct->TxBuff[1] = command + structure->measure_unit;
+    	if(SetupI2CTransmit(TwiStruct, 2))
+    		structure->reg_inst = command;
+    	break;
+    default:
+    	return false;
+    }
+    return true;
+}
+
+bool srf02_read(SRF02_t *structure) {
+	if(timer_tick(&structure->Timeout_Timer)) {
+		structure->busy = false;
+		return false;
+	}
+	if(!structure->busy) return false;
+
+	Twi_t* TwiStruct = structure->TWI;
+	if(structure->addr)
+		TwiStruct->MasterSlaveAddr = structure->addr;
+	else
+		TwiStruct->MasterSlaveAddr = 0x70;
+    switch(structure->reg_inst)
+    {
+    case SRF02_START_RANGING:
+    case SRF02_FAKE_RANGING:
+        TwiStruct->TxBuff[0] = 2;
+        if(!SetupI2CReception(TwiStruct, 1, 2))
+        	return false;
+        else {
+        	structure->range_value = TwiStruct->RxBuff[0] << 8;
+        	structure->range_value |= TwiStruct->RxBuff[1];
+        	return true;
+        }
+    	break;
+    default:
+    	return false;
+    }
+    return true;
+}
+
+void srf02_display_data(SRF02_t *structure) {
+	switch(structure->measure_unit)
+	{
+	case SRF02_INCH:
+		UARTprintf(DebugCom, "SRF02: Range = %u inch\n\r", (unsigned long)structure->range_value);
+		break;
+	case SRF02_CENTIMETER:
+		UARTprintf(DebugCom, "SRF02: Range = %u centimeters\n\r", (unsigned long)structure->range_value);
+		break;
+	case SRF02_MICROSECONDS:
+		UARTprintf(DebugCom, "SRF02: Range = %u microseconds\n\r", (unsigned long)structure->range_value);
+		break;
+	}
+}
+
---
+++ SDK/lib/device/srf02.h
@@ -0,0 +1,54 @@
+/*
+ * srf02.h
+ *
+ *  Created on: Jan 6, 2015
+ *      Author: Morgoth
+ */
+
+#ifndef SRF02_H_
+#define SRF02_H_
+
+#include "stdbool.h"
+#include "api/twi_def.h"
+#include "api/timer_api.h"
+
+typedef enum {
+	SRF02_START_RANGING = 0x50,
+	SRF02_FAKE_RANGING = 0x56,
+	SRF02_8CYCLES_BURST_NO_RANGING = 0x5C,
+	SRF02_FORCE_AUTOTUNE_RESTART = 0x60,
+	SRF02_SEQUENCE_ONE_CHANGE_I2C_ADDR = 0xA0,
+	SRF02_SEQUENCE_THRE_CHANGE_I2C_ADDR = 0xA5,
+	SRF02_SEQUENCE_TWO_CHANGE_I2C_ADDR = 0xAA,
+}SRF02_COMMANDS;
+/*#####################################################*/
+typedef enum {
+	SRF02_INCH = 0,
+	SRF02_CENTIMETER,
+	SRF02_MICROSECONDS,
+}SRF02_UNIT_MEASURE;
+/*#####################################################*/
+typedef struct SRF02_s{
+	STimer_t Timeout_Timer;
+	SRF02_UNIT_MEASURE measure_unit;
+	SRF02_COMMANDS reg_inst;
+	unsigned short range_value;
+	unsigned char addr;
+	bool busy;
+	Twi_t* TWI;
+}SRF02_t;
+bool srf02_start(SRF02_t *structure, SRF02_COMMANDS command);
+bool srf02_read(SRF02_t *structure);
+void srf02_display_data(SRF02_t *structure);
+/*#####################################################*/
+#define new_srf02 SRF02_t
+#ifndef new_
+#define new_(structure) (structure*)calloc(1,sizeof(structure));
+#endif
+#define free_srf02(address) free(address);
+/*#####################################################*/
+#ifdef HEADER_INCLUDE_C_FILES
+#include "srf02.c"
+#endif
+/*#####################################################*/
+#endif /* SRF02_H_ */
