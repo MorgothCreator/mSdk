e2a24fdc0157a91af1682aee3f299ae0f13736ab
Iulian Gheorghiu <morgoth.creator@gmail.com>
2015-01-10 10:33:07 +0200

    Add functionality description to sht11 device driver.

--- SDK/lib/device/sht11.c
+++ SDK/lib/device/sht11.c
@@ -38,12 +38,24 @@ void sht11_delay(SHT11_t *structure) {
 }
 
 bool sht11_write(SHT11_t *structure, unsigned char cmd, unsigned char *status_reg) {
+	/*
+	 * If flag busy is true, signify that a command has been already send
+	 * and now we will wait to read data.
+	 * If flag busy is true will return true, to return signal that the command is send
+	 * and now we will wait for response.
+	 */
 	if(structure->busy) return true;
+	/*
+	 * This is the single case that this function run forward.
+	 * This will go on only if a is called for first time, response is read
+	 * nack is received or timeout is occurred.
+	 * Now will set the timeout in millisecond and send the command.
+	 */
 	timer_interval(&structure->Timeout_Timer, 500);
 	structure->busy = true;
 	gpio_function_set(structure->Scl, GPIO_OUT_OPEN_DRAIN);
 	gpio_function_set(structure->Sda, GPIO_OUT_OPEN_DRAIN);
-	/* Send transmision start
+	/* Send transmission start.
 	 *        ___       ___
 	 * data =    \_____/
 	 *          ___   ___
@@ -94,10 +106,10 @@ bool sht11_write(SHT11_t *structure, unsigned char cmd, unsigned char *status_re
 		return false;
 	}
 	structure->reg_inst = cmd;
-	/*
-	 * Write status register
-	 */
 	if(cmd == SHT11_WRITE_STATUS_REG) {
+		/*
+		 * Write status register
+		 */
 		gpio_function_set(structure->Sda, GPIO_OUT_OPEN_DRAIN);
 		unsigned char st_reg = *status_reg;
 		for(data_cnt = 0; data_cnt < 8; data_cnt++) {
@@ -125,11 +137,11 @@ bool sht11_write(SHT11_t *structure, unsigned char cmd, unsigned char *status_re
 			structure->busy = false;
 			return false;
 		}
-	} else
+		structure->busy = false;
+	} else if(cmd == SHT11_READ_STATUS_REG) {
 		/*
 		 * Read status register.
 		 */
-		if(cmd == SHT11_READ_STATUS_REG) {
 		unsigned char st_reg = 0;
 		for(data_cnt = 0; data_cnt < 8; data_cnt++) {
 			st_reg = st_reg << 1;
@@ -149,10 +161,11 @@ bool sht11_write(SHT11_t *structure, unsigned char cmd, unsigned char *status_re
 		gpio_out(structure->Scl, 0);
 		gpio_function_set(structure->Sda, GPIO_IN_PULL_UP);
 		sht11_delay(structure);
-		unsigned char chk = 0;
+		*status_reg = st_reg;
 		/*
 		 * Read checksum.
 		 */
+		unsigned char chk = 0;
 		for(data_cnt = 0; data_cnt < 8; data_cnt++) {
 			chk = chk << 1;
 			gpio_out(structure->Scl, 1);
@@ -166,18 +179,37 @@ bool sht11_write(SHT11_t *structure, unsigned char cmd, unsigned char *status_re
 		sht11_delay(structure);
 		gpio_out(structure->Scl, 0);
 		sht11_delay(structure);
-		//if(ack) return false;
+		structure->busy = false;
 	}
 	return true;
 }
 
 bool sht11_read(SHT11_t *structure) {
+	/*
+	 * Check if sht11 is not responding.
+	 * If sht11 will not respond until timeout will occurred,
+	 * the busy flag will be set as false to permit to sht11_write function
+	 * to send another command.
+	 */
 	if(timer_tick(&structure->Timeout_Timer)) {
 		structure->busy = false;
 		return false;
 	}
+	/*
+	 * If busy flag is not busy this function will don't have anything to read from sh11.
+	 */
 	if(!structure->busy) return false;
+	/*
+	 * If flag busy is true w will wait to read data.
+	 * Now will wait the sda pin to be '0', that signify data ready to be read.
+	 * If sda pin is '1' will return false, this signify that the sht11 is busy.
+	 */
 	if(gpio_in(structure->Sda)) return false;
+	/*
+	 * Now the data is available on the buss.
+	 * Will start to read data and return true, this is the single case that
+	 * this function return true.
+	 */
 	unsigned char data_cnt;
 	sht11_delay(structure);
 	unsigned short data = 0;
