ad4405a51f7105745267527f571ec331afd996ee
Iulian Gheorghiu <morgoth.creator@gmail.com>
2015-04-09 19:16:56 -0500

    Add AK8975 compass generic driver.

---
+++ SDK/lib/device/ak8975.c
@@ -0,0 +1,74 @@
+/*
+ *  lib/device/ak8975.c
+ *
+ *  Copyright (C) 2013  Iulian Gheorghiu <morgoth.creator@gmail.com>
+ *
+ *  This file is part of Multiplatform SDK.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include "board_init.h"
+#include "ak8975.h"
+#include "api/twi_def.h"
+#include "api/timer_api.h"
+
+
+bool ak8975_start_measure(AK8975_t *structure) {
+	if(!structure->TWI) return false;
+	Twi_t *TwiStruct = structure->TWI;
+	TwiStruct->MasterSlaveAddr = AK8975_ADDR | (structure->IcNr & 0x03);
+	TwiStruct->TxBuff[0] = AK8975_CNTL_REG;
+	TwiStruct->TxBuff[1] = AK8975_CTRL_MODE_SINGLE;
+	if(!SetupI2CTransmit(TwiStruct, 2)) return false;
+	return true;
+}
+
+bool ak8975_ready(AK8975_t *structure) {
+	Twi_t *TwiStruct = structure->TWI;
+	TwiStruct->MasterSlaveAddr = AK8975_ADDR | (structure->IcNr & 0x03);
+	TwiStruct->TxBuff[0] = AK8975_ST1_REG;
+	if(!SetupI2CReception(TwiStruct, 1, 1)) return false;
+	if(TwiStruct->RxBuff[0] && AK8975_ST1_DREADY_bm)
+		return true;
+	else
+		return false;
+}
+
+bool ak8975_read_data(AK8975_t *structure, signed short *X_Axis, signed short *Y_Axis, signed short *Z_Axis) {
+	Twi_t *TwiStruct = structure->TWI;
+	TwiStruct->MasterSlaveAddr = AK8975_ADDR | (structure->IcNr & 0x03);
+	TwiStruct->TxBuff[0] = AK8975_HXL_REG;
+	if(!SetupI2CReception(TwiStruct, 1, 6)) return false;
+	*X_Axis = (signed short)((TwiStruct->RxBuff[1] << 8) + TwiStruct->RxBuff[0]);
+	*Y_Axis = (signed short)((TwiStruct->RxBuff[3] << 8) + TwiStruct->RxBuff[2]);
+	*Z_Axis = (signed short)((TwiStruct->RxBuff[5] << 8) + TwiStruct->RxBuff[4]);
+	return true;
+}
+
+bool ak8975_display_result(AK8975_t *structure) {
+	signed short Xg = 0;
+	signed short Yg = 0;
+	signed short Zg = 0;
+	ak8975_start_measure(structure);
+	while(!ak8975_ready(structure)) {
+		//Sysdelay(10);
+	}
+	if(!ak8975_read_data(structure, &Xg, &Yg, &Zg)) return false;
+	UARTprintf(DebugCom, "AK8975 Magnetometer:\n\rXg = %d, Yg = %d, Zg = %d\n\r", Xg, Yg, Zg);
+	return true;
+}
+
+
---
+++ SDK/lib/device/ak8975.h
@@ -0,0 +1,124 @@
+/*
+ *  lib/device/ak8975.h
+ *
+ *  Copyright (C) 2013  Iulian Gheorghiu <morgoth.creator@gmail.com>
+ *
+ *  This file is part of Multiplatform SDK.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*#####################################################*/
+#ifndef AK8975_H_
+#define AK8975_H_
+/*#####################################################*/
+#include <stdbool.h>
+#include "api/twi_def.h"
+/*#####################################################*/
+#define AK8975_ADDR					(0x0C)
+
+/*
+ * Read-only registers
+ */
+#define AK8975_WIA_REG				(0x00)
+#define AK8975_INFO_REG				(0x01)
+#define AK8975_ST1_REG				(0x02)
+#define AK8975_HXL_REG				(0x03)
+#define AK8975_HXH_REG				(0x04)
+#define AK8975_HYL_REG				(0x05)
+#define AK8975_HYH_REG				(0x06)
+#define AK8975_HZL_REG				(0x07)
+#define AK8975_HZH_REG				(0x08)
+#define AK8975_ST2_REG				(0x09)
+/*
+ * Write/Read registers
+ */
+#define AK8975_CNTL_REG				(0x0A)
+#define AK8975_RSV_REG				(0x0B)
+#define AK8975_ASTC_REG				(0x0C)
+#define AK8975_TS1_REG				(0x0D)
+#define AK8975_TS2_REG				(0x0E)
+#define AK8975_I2CDIS_REG			(0x0F)
+/*
+ * Read-only registers
+ */
+#define AK8975_ASAX_REG				(0x10)
+#define AK8975_ASAY_REG				(0x11)
+#define AK8975_ASAZ_REG				(0x12)
+
+/*
+ * ST1 reg
+ */
+#define AK8975_ST1_DREADY_bp		0
+#define AK8975_ST1_DREADY_bm		(0x01 << AK8975_ST1_DREADY_bp)
+
+/*
+ * ST2 reg
+ */
+#define AK8975_ST2_HOLF_bp			3	/* Magnetic sensor overflow */
+#define AK8975_ST2_HOLF_bm			(0x01 << AK8975_ST2_HOLF_bp)
+
+#define AK8975_ST2_DERR_bp			3	/* Data error occured */
+#define AK8975_ST2_DERR_bm			(0x01 << AK8975_ST2_DERR_bp)
+
+/*
+ * CTRL reg
+ */
+#define AK8975_CTRL_MODE_gp			0	/* Data error occured */
+#define AK8975_CTRL_MODE_gm			(0x0F << AK8975_CTRL_MODE_gp)
+#define AK8975_CTRL_MODE_PWRDN		0x00
+#define AK8975_CTRL_MODE_SINGLE		0x01
+#define AK8975_CTRL_MODE_SELFTEST	0x08
+#define AK8975_CTRL_MODE_ROM		0x0F
+
+/*
+ * ASTC reg
+ */
+#define AK8975_ASTC_SELF_bp			6	/* Data error occured */
+#define AK8975_ASTC_SELF_bm			(0x01 << AK8975_ASTC_SELF_bp)
+
+/*
+ * I2CDIS reg
+ */
+#define AK8975_I2CDIS_bp			0	/* Data error occured */
+#define AK8975_I2CDIS_bm			(0x01 << AK8975_I2CDIS_bp)
+
+/*#####################################################*/
+typedef struct AK8975_s{
+	//STimer_t Timeout_Timer;
+	bool busy;
+	Twi_t* TWI;
+	unsigned char IcNr;
+	unsigned char Stage;
+}AK8975_t;
+
+bool ak8975_start_measure(AK8975_t *structure);
+bool ak8975_ready(AK8975_t *structure);
+bool ak8975_read_data(AK8975_t *structure, signed short *X_Axis, signed short *Y_Axis, signed short *Z_Axis);
+bool ak8975_display_result(AK8975_t *structure);
+
+/*#####################################################*/
+#define new_ak8975 AK8975_t
+#ifndef new_
+#define new_(structure) (structure*)calloc(1,sizeof(structure));
+#endif
+#define free_ak8975(address) free(address);
+/*#####################################################*/
+#ifdef HEADER_INCLUDE_C_FILES
+#include "ak8975.c"
+#endif
+
+/*#####################################################*/
+#endif
+/*#####################################################*/
