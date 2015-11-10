e7a4bbeafbfca813f3be0c58c53d59548b0f3c2f
Iulian Gheorghiu <morgoth.creator@gmail.com>
2015-11-09 22:35:11 -0500

    Multiple changes....... see details.
    Changed MMCSD api card detect pin to Gpio_t assigned structure.
    Changed LED's and HARDBTN's pointers to array of pointers.
    Add customization of LED's and HARDBTN's from main.h.
    Add MmcSd 0 and 1 initialization, on BBB will mount onboard eMMC
    storage.

--- SDK_Probe_AM335x/TMDSSK3358_Test/board_init.c
+++ SDK_Probe_AM335x/TMDSSK3358_Test/board_init.c
@@ -9,6 +9,7 @@
 
 
 #include "board_init.h"
+#include "main.h"
 
 #include "api/core_init_api.h"
 #include "api/uart_api.h"
@@ -17,6 +18,7 @@
 #include "api/lcd_def.h"
 #include "api/lcd_api.h"
 #include "api/touchscreen_def.h"
+#include "api/mmcsd_api.h"
 
 #include "lib/gfx/controls_definition.h"
 #include "lib/fs/fat.h"
@@ -30,11 +32,22 @@
 #include "interface/int_touchscreen_interface.h"
 #include "interface/rtc_interface.h"
 
+#include "lib/fs/fat.h"
+
+#include "interface/mmcsdlib/mmcsd_proto.h"
+
 new_uart* DebugCom = NULL;
 new_twi* TWI0 = NULL;
 new_mcspi* MCSPI0 = NULL;
 new_touchscreen* TouchScreen = NULL;
 new_screen* ScreenBuff = NULL;
+new_gpio* LED[LEDS_NR] = {NULL};
+new_gpio* HARDBTN[HARDBTNS_NR] = {NULL};
+
+
+new_gpio* MmcSd_Present = NULL;
+new_gpio* eMMC_Res = NULL;
+mmcsdCtrlInfo sdCtrl[2];
 
 inline bool board_init()
 {
@@ -59,6 +72,36 @@ inline bool board_init()
 	/* Display board message*/
 #if defined(BOARD_MESSAGE)
 	UARTprintf(DebugCom, "Use %s Board.\n\r", BOARD_MESSAGE);
+#endif
+/*-----------------------------------------------------*/
+#if defined(HARDBTN1_PORT) && defined(HARDBTN1_PIN)
+	HARDBTN[0] = gpio_assign(HARDBTN1_PORT, HARDBTN1_PIN, GPIO_DIR_INPUT, false);
+	gpio_up_dn(HARDBTN[0], 0);
+#endif
+#if defined(HARDBTN2_PORT) && defined(HARDBTN2_PIN)
+	HARDBTN[1] = gpio_assign(HARDBTN2_PORT, HARDBTN2_PIN, GPIO_DIR_INPUT, false);
+	gpio_up_dn(HARDBTN[1], 0);
+#endif
+#if defined(HARDBTN3_PORT) && defined(HARDBTN3_PIN)
+	HARDBTN[2] = gpio_assign(HARDBTN3_PORT, HARDBTN3_PIN, GPIO_DIR_INPUT, false);
+	gpio_up_dn(HARDBTN[2], 0);
+#endif
+#if defined(HARDBTN4_PORT) && defined(HARDBTN4_PIN)
+	HARDBTN[3] = gpio_assign(HARDBTN4_PORT, HARDBTN4_PIN, GPIO_DIR_INPUT, false);
+	gpio_up_dn(HARDBTN[3], 0);
+#endif
+/*-----------------------------------------------------*/
+#if defined(LED1_PORT) && defined(LED1_PIN)
+	LED[0] = gpio_assign(LED1_PORT, LED1_PIN, GPIO_DIR_OUTPUT, false);
+#endif
+#if defined(LED2_PORT) && defined(LED2_PIN)
+	LED[1] = gpio_assign(LED2_PORT, LED2_PIN, GPIO_DIR_OUTPUT, false);
+#endif
+#if defined(LED3_PORT) && defined(LED3_PIN)
+	LED[2] = gpio_assign(LED3_PORT, LED3_PIN, GPIO_DIR_OUTPUT, false);
+#endif
+#if defined(LED4_PORT) && defined(LED4_PIN)
+	LED[3] = gpio_assign(LED4_PORT, LED4_PIN, GPIO_DIR_OUTPUT, false);
 #endif
 /*-----------------------------------------------------*/
 /* Set up the Twi 0 to communicate with PMIC and the Onboard serial EEprom memory */
@@ -87,13 +130,30 @@ inline bool board_init()
 	MCSPI0->MosiPort = IOB;
 	MCSPI0->MisoPort = IOB;
 	MCSPI0->SckPort = IOA;
-	MCSPI0->Cs0Port = IOA;
+	MCSPI0->CsPort[0] = IOA;
 	MCSPI0->MosiPin = 17;
 	MCSPI0->MisoPin = 16;
 	MCSPI0->SckPin = 17;
-	MCSPI0->Cs0Pin = 16;
+	MCSPI0->CsPin[0] = 16;
 	mcspi_open(MCSPI0);
 	UARTPuts(DebugCom, "OK.\n\r" , -1);
+/*-----------------------------------------------------*/
+	MmcSd_Present = gpio_assign(0, 6, GPIO_DIR_OUTPUT, false);
+	UARTPuts(DebugCom, "Init MMCSD0 .......", -1);
+	sdCtrl[0].SdNr = 0;
+	mmcsd_init(&sdCtrl[0], MmcSd_Present, LED[0]);
+	UARTPuts(DebugCom, "OK.\n\r", -1);
+	mmcsd_idle(&sdCtrl[0]);
+/*-----------------------------------------------------*/
+	eMMC_Res = gpio_assign(1, 20, GPIO_DIR_OUTPUT, false);
+	gpio_out(eMMC_Res, 0);
+	Sysdelay(10);
+	gpio_out(eMMC_Res, 1);
+	UARTPuts(DebugCom, "Init eMMC on MMCSD1 interface.......", -1);
+	sdCtrl[1].SdNr = 1;
+	mmcsd_init(&sdCtrl[1], (Gpio_t*)NULL, LED[0]);
+	UARTPuts(DebugCom, "OK.\n\r", -1);
+	mmcsd_idle(&sdCtrl[1]);
 /*-----------------------------------------------------*/
 	ScreenBuff = new_(new_screen);
 	ScreenBuff->raster_timings = &lcd_TFT43_TMDSSK3358;
--- SDK_Probe_AM335x/TMDSSK3358_Test/board_init.h
+++ SDK_Probe_AM335x/TMDSSK3358_Test/board_init.h
@@ -10,9 +10,45 @@
 /*#####################################################*/
 #include <stdbool.h>
 #include <string.h>
+#include "api/core_init_api.h"
 #include "api/uart_api.h"
+#include "api/twi_api.h"
+#include "api/mcspi_api.h"
+#include "api/lcd_def.h"
+#include "api/lcd_api.h"
+#include "api/touchscreen_def.h"
+#include "api/mmcsd_api.h"
+
+#include "lib/gfx/controls_definition.h"
+#include "lib/fs/fat.h"
+
+#include "device/ADXL345.h"
+
+#include "pinmux/pin_mux_uart.h"
+#include "pinmux/pin_mux_spi.h"
+#include "pinmux/pin_mux_twi.h"
+
+#include "interface/int_touchscreen_interface.h"
+#include "interface/rtc_interface.h"
+
+#include "lib/fs/fat.h"
+
+#include "interface/mmcsdlib/mmcsd_proto.h"
+
 
 extern new_uart* DebugCom;
+extern new_twi* TWI0;
+extern new_mcspi* MCSPI0;
+extern new_touchscreen* TouchScreen;
+extern new_screen* ScreenBuff;
+extern new_gpio* LED[];
+extern new_gpio* HARDBTN[];
+
+
+extern new_gpio* MmcSd_Present;
+extern new_gpio* eMMC_Res;
+extern mmcsdCtrlInfo sdCtrl[];
+
 
 inline bool board_init();
 
