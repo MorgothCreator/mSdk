70f19afd2f7f647b3b2121aa3cb3a056a59b6c0c
Iulian Gheorghiu <morgoth.creator@gmail.com>
2015-11-09 22:30:37 -0500

    Add custon LED's and Hard buttons customization in main.h

---
+++ SDK_Probe_AM335x/TMDSSK3358_Test/main.h
@@ -0,0 +1,39 @@
+/*
+ * main.h
+ *
+ *  Created on: Nov 9, 2015
+ *      Author: John Smith
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define LED1_PORT		1
+#define LED1_PIN		7
+
+#define LED2_PORT		1
+#define LED2_PIN		6
+
+#define LED3_PORT		1
+#define LED3_PIN		5
+
+#define LED4_PORT		1
+#define LED4_PIN		4
+
+#define LEDS_NR			4
+
+#define HARDBTN1_PORT	0
+#define HARDBTN1_PIN	30
+
+#define HARDBTN2_PORT	2
+#define HARDBTN2_PIN	3
+
+#define HARDBTN3_PORT	2
+#define HARDBTN3_PIN	2
+
+#define HARDBTN4_PORT	2
+#define HARDBTN4_PIN	5
+
+#define HARDBTNS_NR		4
+
+#endif /* MAIN_H_ */
