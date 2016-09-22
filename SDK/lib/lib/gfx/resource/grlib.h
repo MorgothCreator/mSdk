7f8991db411bfb72ba43c587fe1555dbed2e24ba
Ghe <morgoth.creator@gmail.com>
2016-09-22 03:47:13 -0600

    Fix compiler error vhen use C++.

--- SDK/lib/lib/gfx/resource/grlib.h
+++ SDK/lib/lib/gfx/resource/grlib.h
@@ -37,12 +37,9 @@
 //
 // If building with a C++ compiler, make all of the definitions in this header
 // have a C binding.
-//
-//*****************************************************************************
+//*******************************
 #ifdef __cplusplus
-extern "C"
-{
-#endif
+//**********************************************
 
 
 #endif // __GRLIB_H__
