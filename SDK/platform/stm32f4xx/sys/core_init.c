4127ea7b49cf5c5ee8c44112bb565e6a2c40cc01
Ghe <morgoth.creator@gmail.com>
2016-11-06 23:03:23 -0800

    Add clock settings for STM32F469 and STM32F479 devices.

--- SDK/platform/stm32f4xx/sys/core_init.c
+++ SDK/platform/stm32f4xx/sys/core_init.c
@@ -10,13 +10,103 @@
 #include "board_properties.h"
 #include "api/gpio_api.h"
 #include "sys/system_stm32f4xx.h"
+#include "driver/stm32f4xx_hal.h"
 #include "driver/stm32f4xx_hal_rcc.h"
+#include "driver/stm32f4xx_hal_pwr_ex.h"
+#include "driver/stm32f4xx_hal_flash_ex.h"
 //#include "driver/core.h"
+
+#if defined(STM32F469xx) || defined(STM32F479xx)
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 180000000
+  *            HCLK(Hz)                       = 180000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 8
+  *            PLL_N                          = 360
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 7
+  *            PLL_R                          = 6
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 5
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  HAL_StatusTypeDef ret = HAL_OK;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* The voltage scaling allows optimizing the power consumption when the device is
+     clocked below the maximum system frequency, to update the voltage scaling value
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+#if defined(USE_STM32469I_DISCO_REVA)
+  RCC_OscInitStruct.PLL.PLLM = 25;
+#else
+  RCC_OscInitStruct.PLL.PLLM = 8;
+#endif /* USE_STM32469I_DISCO_REVA */
+  RCC_OscInitStruct.PLL.PLLN = 360;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  RCC_OscInitStruct.PLL.PLLR = 6;
+
+  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+
+  /* Activate the OverDrive to reach the 180 MHz Frequency */
+  ret = HAL_PWREx_EnableOverDrive();
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+}
+#endif
 
 void _core_init(void)
 {
 	//SystemInit();
+#if !defined(STM32F469xx) && !defined(STM32F479xx)
 	SystemCoreClockUpdate();
+#else
+#include "driver/stm32469i_discovery_sdram.h"
+	SystemClock_Config();
+	BSP_STM32469I_DISCOVERY_SDRAM_Init();
+#endif
+	HAL_Init();
+	//SystemCoreClockUpdate();
 	//RCC_PCLK1Config(RCC_HCLK_Div4);
 	//RCC_PCLK2Config(RCC_HCLK_Div2);
 	gpio_init(0);
@@ -28,4 +118,6 @@ void _core_init(void)
 	gpio_init(6);
 	gpio_init(7);
 	gpio_init(8);
+	gpio_init(9);
+	gpio_init(10);
 }
